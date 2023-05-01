e8f629ef34f0ac4955ebb4841a1924c401794db9
DmytroKrylov <120196125+DmytroKrylov@users.noreply.github.com>
2023-05-01 17:47:54 +0300

    Update

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,18 @@
-const categories = document.querySelectorAll("li.item");
-console.log("Number of categories:", categories.length);
+// const categories = document.querySelectorAll("li.item");
+// console.log("Number of categories:", categories.length);
 
-categories.forEach((category) => {
-  const categoriesNames = category.querySelector("h2").textContent;
-  const categoriesElements = category.querySelectorAll("li").length;
-  console.log("Category:", categoriesNames);
-  console.log("Elements:", categoriesElements);
+// categories.forEach((category) => {
+//   const categoriesNames = category.querySelector("h2").textContent;
+//   const categoriesElements = category.querySelectorAll("li").length;
+//   console.log("Category:", categoriesNames);
+//   console.log("Elements:", categoriesElements);
+// });
+const categories = document.querySelector("#categories");
+const items = document.querySelectorAll(".item");
+
+console.log("Number of categories:", categories.children.length);
+
+items.forEach((item) => {
+  console.log("Category:", item.firstElementChild.textContent);
+  console.log("Elements:", item.lastElementChild.children.length);
 });
--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,16 @@
+// const input = document.getElementById("font-size-control");
+// const span = document.getElementById("text");
+
+// input.addEventListener("input", () => {
+//   const fontSize = input.value + "px";
+//   span.style.fontSize = fontSize;
+// });
+
 const input = document.getElementById("font-size-control");
 const span = document.getElementById("text");
+
+const initialFontSize = input.value + "px";
+span.style.fontSize = initialFontSize;
 
 input.addEventListener("input", () => {
   const fontSize = input.value + "px";
